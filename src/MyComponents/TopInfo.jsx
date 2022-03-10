017d497cd934ad44c16873960bddc02695fe6e0a
Chelea Mihail <mikeskin10@gmail.com>
2022-03-10 19:14:28 +0200

    Optimizarea stilurilor

--- src/MyComponents/TopInfo.jsx
+++ src/MyComponents/TopInfo.jsx
@@ -2,35 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Container, Typography, Grid, Button } from '@material-ui/core';
 const useStyles = makeStyles((theme) =>({
-    root:{
-      flexGrow: 1
-    },
     menuButton:{
       marginRight: theme.spacing(1)
-    },
-    title:{
-      flexGrow:1
-    },
-    mainFeaturesPost:{
-      position: "relative",
-      color: theme.palette.common.white,
-      marginBottom: theme.spacing(4),
-      backgroundSize: "cover",
-      backgroundRepeat:"no-repeat",
-      backgroundPosition: "center"
-    },
-    mainFeaturesPostContent:{
-      position: "relative",
-      padding: theme.spacing(6),
-      marginTop: theme.spacing(3)
-    },
-    overlay:{
-      position: "absolute",
-      top:0,
-      bottom:0,
-      left:0,
-      right:0,
-      backgroundOverlay:"rgba(0,0,0,.3)"
     }
     }))
 
