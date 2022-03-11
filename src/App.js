5e3f3d09ac63cda03d964613ca3b67f88d22297c
Chelea Mihail <mikeskin10@gmail.com>
2022-03-11 14:18:52 +0200

    Schimbarea denumirilor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from 'react';
+
 import './App.css';
-import MyNavigation from './MyComponents/MyHeader/MyNavigation';
-import MyHeader from './MyComponents/MyHeader';
+import MyNavigation from './MyComponents/Navigation';
+import MyHeader from './MyComponents/Header';
 import TopInfo from './MyComponents/TopInfo';
 import Body from './MyComponents/Body';
-import MyFooter from './MyComponents/MyFooter';
+import MyFooter from './MyComponents/Footer';
 
 function App() {
   return (
     <div className="App">
-<MyNavigation></MyNavigation>
-<MyHeader></MyHeader>
-<TopInfo></TopInfo>
-<Body></Body>
-<MyFooter></MyFooter>
+      <MyNavigation></MyNavigation>
+      <MyHeader></MyHeader>
+      <TopInfo></TopInfo>
+      <Body></Body>
+      <MyFooter></MyFooter>
     </div>
   );
 }
